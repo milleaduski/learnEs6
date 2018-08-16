09077efc91988c3e30171dac93ba851d378ebaa9
millea <millea1994@gmail.com>
2018-08-17 02:51:18 +0700

    string litera , shorthand and destructuring object

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,12 @@
-"use strict";
+'use strict';
 
-// const array = ["Duski","Millea","Aliandra","Shorter"];
-// //foreach data
-// array.forEach(data=>{
-// 	console.log(data)
-// })
+//Destructuring
+var obj = {
+	name: 'Millea',
+	lastname: 'Duski'
+	//assign new value
+};var myLastName = obj.lastname,
+    newName = obj.name;
+//
 
-var restSpread = function restSpread(param1, param2, param3) {
-  console.log("My name: " + param1 + " Lastname:" + param2 + " Age :" + param3);
-};
-//rest and spread
-var fullname = ["Millea", "Duski harahap", 21];
-restSpread.apply(undefined, fullname);
+console.log(newName, myLastName);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,48 @@
 // 	console.log(data)
 // })
 
-let restSpread = (param1, param2, param3)=>{
-console.log("My name: " + param1 + " Lastname:" + param2 + " Age :" + param3)
+// let restSpread = (param1, param2, param3)=>{
+// console.log("My name: " + param1 + " Lastname:" + param2 + " Age :" + param3)
+// }
+// //rest and spread
+// let fullname = ["Millea","Duski harahap", 21]
+// restSpread(...fullname)
+// const name = "Duski"
+// func = (string,variable)=>{
+//  console.log(string);
+// }
+// let data = func`My name is ${name}`
+
+//shorthand in obj
+// const name = "Millea";
+// const lastname = "Duski"
+
+// let obj = {
+// 	name, // it means name = name
+// 	lastname
+// }
+
+// console.log(obj)
+
+// //shorthand in function
+// const name = "Millea";
+// const lastname = "Duski"
+
+// let funcData = function getData(){
+// 	return `My name is ${name} ${lastname}`
+// }
+// let obj = {
+// 	funcData
+// }
+// //
+// console.log(obj.funcData())
+
+//Destructuring
+let obj = {
+	name : 'Millea',
+	lastname : 'Duski'
 }
-//rest and spread
-let fullname = ["Millea","Duski harahap", 21]
-restSpread(...fullname)
+//assign new value
+const {lastname : myLastName, name : newName} = obj
+//
+console.log(myLastName, newName)
